2ea46be7b23d94a58a7985c9b34645bcbcfea9df
HenriqueFutema <minifut0407@gmail.com>
2019-08-16 14:38:06 -0300

    Model Order

---
+++ orders/src/Models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+    customerId: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+
+    book: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true
+    },
+
+    initialDate: {
+        type: Date,
+        required: true
+    },
+
+    deliveryDate: {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
--- orders/src/server.js
+++ orders/src/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+const config = require("./config/database");
+
+mongoose.connect(config.url, {
+    useNewUrlParser: true
+});
+
 app.listen(3002, () => {
     console.log("Orders Porta");
 });
